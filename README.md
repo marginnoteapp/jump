d33da2138c63762a6a8808795312a17af9a7d238
ourongxing <orongxing@gmail.com>
2022-09-22 00:32:46 +0800

    release 0.9.1

--- README.md
+++ README.md
@@ -6,11 +6,10 @@
 <p align="center">
   <a href="https://github.com/marginnoteapp/jump/network/members"><img src="https://img.shields.io/github/forks/marginnoteapp/jump.svg?style=flat" alt="forks"></a>
   <a href="https://github.com/marginnoteapp/jump/stargazers"><img src="https://img.shields.io/github/stars/marginnoteapp/jump.svg?style=flat" alt="stars"></a>
-  <a href="https://github.com/marginnoteapp/jump/blob/main/package.json"><img src="https://img.shields.io/badge/version-v0.9.0-orange" alt="version"></a>
+  <a href="https://github.com/marginnoteapp/jump/blob/main/package.json"><img src="https://img.shields.io/badge/version-v0.9.1-orange" alt="version"></a>
   <a href="https://github.com/marginnoteapp/jump/blob/main/LICENSE"><img src="https://img.shields.io/badge/license-MIT-green" alt="license"></a>
 </p>
 
-
 ## License
 
-[MIT](https://github.com/marginnoteapp/jump/blob/main/LICENSE)
+<a href="https://github.com/marginnoteapp/jump/blob/main/LICENSE">MIT</a> © <a href="https://github.com/marginnoteapp"><img src="https://testmnbbs.oss-cn-zhangjiakou.aliyuncs.com/pic/mn.png?x-oss-process=base_webp" alt="MarginNote" width="80"></a>
--- main.js
+++ main.js
@@ -1,109 +1,143 @@
-;(function () {
+/**
+ * MIT License
+ * Copyright © 2022 MarginNote
+ * Github: https://github.com/marginnoteapp/jump
+ * Welcom to contribute to this project!
+ */
+
+try {
   const zh = {
-    confirm: '确定',
-    cancel: '取消',
-    enterOffset: '请在 MetaData -> 页码偏移量中输入当前文档的页码偏移量',
+    confirm: "确定",
+    cancel: "取消",
+    no_metaData: "检测到您并未安装 MetaData 插件，无法设置页码偏移量",
     offset: offset =>
-      `当前文档的页码偏移量为 ${offset}，请输入想要去的页码，我会自动计算。`,
+      offset
+        ? `当前文档的页码偏移量为 ${offset}`
+        : "当前文档的页码偏移量为 0，可以前去 MetaData 插件中设置页码偏移量",
+    download: "下载 MetaData",
+    deleted: "该页已删除",
+    out_range: "超出本书页码范围",
     gotopage: `转到指定页码`,
-    enterInteger: '请输入整数'
+    enter_integer: "请输入整数"
   }
   const en = {
-    confirm: 'Confirm',
-    cancel: 'Cancel', enterOffset:
-      'Please enter the page offset of the current document in MetaData-> Page Offset',
+    confirm: "Confirm",
+    cancel: "Cancel",
+    deleted: "This page has been deleted",
+    download: "Download MetaData",
+    out_range: "Out of page range of this book",
+    no_metaData:
+      "You have not installed MetaData addon, so you can't set the page offset",
     offset: offset =>
-      `The current document's page offset is ${offset}, please enter the page number you want to go and I will calculate it automatically.`,
-    gotopage: 'Go To Page',
-    enterInteger: 'Please enter an integer'
+      offset
+        ? `This document's page offset is ${offset}`
+        : "This document's page offset is 0, you can go to MetaData addon to set the page offset",
+    gotopage: "Go To Page",
+    enter_integer: "Please enter an integer"
   }
   JSB.newAddon = () => {
     const Addon = {
-      name: 'Jump',
-      key: 'jump'
+      name: "Jump",
+      key: "jump"
     }
-    let pageOffsets = {}
     const lang =
       NSLocale.preferredLanguages().length &&
-      NSLocale.preferredLanguages()[0].startsWith('zh')
+      NSLocale.preferredLanguages()[0].startsWith("zh")
         ? zh
         : en
     const console = {
-      log(obj) {
-        JSB.log(`${Addon.key} %@`, obj)
+      log(obj, suffix = "normal") {
+        JSB.log(`${Addon.key}-${suffix} %@`, obj)
       }
     }
-    const getPageOffset = (md5) => {
-      const offset = NSUserDefaults.standardUserDefaults().objectForKey('metadata_profile_doc')?.[md5]?.addon?.pageOffset
-      if(offset) return offset
+    function getPageOffset(md5) {
+      return NSUserDefaults.standardUserDefaults().objectForKey(
+        "metadata_profile_doc"
+      )?.[md5]?.addon?.pageOffset
     }
-    const popup = (title, message, buttons = [lang.confirm]) => {
+    function popup(title, message, type, buttons = [lang.confirm]) {
       return new Promise(resolve =>
         UIAlertView.showWithTitleMessageStyleCancelButtonTitleOtherButtonTitlesTapBlock(
           title,
           message,
-          2,
+          type,
           lang.cancel,
           buttons,
           (alert, buttonIndex) => {
-            resolve({
-              content: alert.textFieldAtIndex(0).text.trim(),
-              option: buttonIndex - 1
-            })
+            resolve(
+              type === 2
+                ? {
+                    content: alert.textFieldAtIndex(0).text.trim(),
+                    option: buttonIndex - 1
+                  }
+                : {
+                    option: buttonIndex - 1
+                  }
+            )
           }
         )
       )
     }
-    const showHUD = (text, duration = 2) => {
+    function showHUD(text, duration = 2) {
       Application.sharedInstance().showHUD(text, self.window, duration)
     }
-    const go = async () => {
-      try {
-        let offset = getPageOffset(self.docmd5) ?? pageOffsets[self.docmd5]
-        if (offset === undefined) {
-          showHUD(lang.enterOffset, 3)
-          return
+    function openUrl(url) {
+      Application.sharedInstance().openURL(NSURL.URLWithString(encodeURI(url)))
+    }
+    async function jump() {
+      let offset = getPageOffset(self.docmd5)
+      if (offset === undefined) {
+        const { option } = await popup("Jump", lang.no_metaData, 0, [
+          lang.download
+        ])
+        if (option === 0) {
+          openUrl("https://github.com/marginnoteapp/jump")
         }
-        for (;;) {
-          const { option, content } = await popup(
-            'Jump',
-            lang.offset(offset)
-          )
-          if (option === -1) return
-          const page = Number(content)
-          if (Number.isInteger(page)) {
-            const documentController = self.studyController.readerController.currentDocumentController
-            documentController.setPageAtIndex(documentController.indexFromPageNo(page + Number(offset)))
-            return
+        return
+      }
+      offset = Number(offset)
+      for (;;) {
+        const { option, content } = await popup("Jump", lang.offset(offset), 2)
+        if (option === -1) return
+        const page = Number(content)
+        if (Number.isInteger(page)) {
+          const realPage = page + offset
+          const index = self.documentController.indexFromPageNo(realPage)
+          if (
+            realPage > self.documentController.document.pageCount ||
+            realPage < 1
+          ) {
+            showHUD(lang.out_range)
+          } else if (index === 0 && realPage !== 1) {
+            showHUD(lang.deleted)
           } else {
-            showHUD(lang.enterInteger)
+            self.documentController.setPageAtIndex(index)
+            return
           }
+        } else {
+          showHUD(lang.enter_integer)
         }
-      } catch (err) {
-        console.log(String(err))
       }
     }
     return JSB.defineClass(
-      Addon.name + ': JSExtension',
+      Addon.name + ": JSExtension",
       {
         sceneWillConnect() {
           self.status = false
           self.app = Application.sharedInstance()
           self.studyController = self.app.studyController(self.window)
-          pageOffsets =
-            NSUserDefaults.standardUserDefaults().objectForKey(
-              'GoToPage.Offsets'
-            ) ?? {}
         },
-        documentDidOpen (docmd5) {
+        documentDidOpen(docmd5) {
           self.docmd5 = docmd5
+          self.documentController =
+            self.studyController.readerController.currentDocumentController
         },
         queryAddonCommandStatus() {
           return self.studyController.studyMode !== 3
             ? {
-                image: 'logo.png',
+                image: "logo.png",
                 object: self,
-                selector: 'onToggle:',
+                selector: "onToggle:",
                 checked: self.status
               }
             : null
@@ -111,7 +145,7 @@
         async onToggle() {
           self.status = true
           self.studyController.refreshAddonCommands()
-          await go()
+          await jump()
           self.status = false
           self.studyController.refreshAddonCommands()
         }
@@ -119,4 +153,6 @@
       {}
     )
   }
-})()
+} catch (err) {
+  JSB.log(`jump-error %@`, String(err))
+}
